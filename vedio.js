5154032fe49fdb27fe06adf7a3cfdeceb7f6bf40
ehasun15019 <ehasunulislam0202@gmail.com>
2026-09-12 22:01:11 +0600

    vedio.ks

---
+++ vedio.js
@@ -0,0 +1,25 @@
+// let Name = "Orko";
+
+// function greet() {
+//   console.log(Name);
+// }
+
+// greet();
+
+
+// function greet() {
+//   let message = "Hello";
+//   console.log(message);
+// }
+
+// greet();
+
+// console.log(message);
+
+
+// if (true) {
+//   let age = 22;
+//   const city = "Dhaka";
+// }
+
+// console.log(age);
